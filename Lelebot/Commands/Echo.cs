7b794cb1ed7b722da6a698fbcd112e4db0a8411d
Phill <philldamaskin@gmail.com>
2019-08-03 19:14:14 -0400

    initial commit

---
+++ Lelebot/Commands/Echo.cs
@@ -0,0 +1,22 @@
+﻿namespace Lelebot.Commands
+{
+    public class Echo : Command
+    {
+        public override bool Match(Context context)
+        {
+            if (context.Command == "echo")
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public override void Run()
+        {
+            Print(Context.Text);
+        }
+    }
+}
---
+++ Lelebot/Context.cs
@@ -0,0 +1,13 @@
+﻿using Discord.WebSocket;
+
+namespace Lelebot
+{
+    public class Context
+    {
+        public SocketUser Author { get; set; }
+        public ISocketMessageChannel Channel { get; set; }
+        public string Text { get; set; }
+        public string[] Args { get; set; }
+        public string Command { get; set; }
+    }
+}
