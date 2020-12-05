21a54159aa33605ceddc74bf8bfb262193b2959b
Phill <philldamaskin@gmail.com>
2020-12-05 18:36:30 -0500

    fix py command

---
+++ Lelebot/Commands/RunPython.cs
@@ -0,0 +1,35 @@
+﻿using IronPython.Hosting;
+using Microsoft.Scripting.Hosting;
+using System.Threading.Tasks;
+
+namespace Lelebot.Commands
+{
+    public class RunPython : IBaseCommand, IHelp
+    {
+        string IHelp.Help => "Executes a line of python code";
+        string IBaseCommand.BaseCommand => "py";
+
+        async Task<Message> ICommand.Run(Call call)
+        {
+            await Task.CompletedTask;
+            if (call.RawText.Length > 4)
+            {
+                string text = call.RawText.Substring(3);
+                try
+                {
+                    Message message = new();
+                    ScriptEngine pythonEngine = Python.CreateEngine();
+                    ScriptSource pythonScript = pythonEngine.CreateScriptSourceFromString(text);
+                    message.Append(pythonScript.Execute());
+                    return message;
+                }
+                catch
+                {
+                    return null;
+                }
+            }
+
+            return null;
+        }
+    }
+}
