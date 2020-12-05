ed6dd6e1072be8d8460271dc88137d621d63270a
Phill <philldamaskin@gmail.com>
2020-12-05 14:49:17 -0500

    add launcher project

---
+++ Updater/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Updater
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            if (args.Length == 1)
+            {
+                string pathToExecutable = args[0];
+                Console.WriteLine($"Path = {pathToExecutable}");
+            }
+            else
+            {
+                Console.WriteLine("Missing argument for the bot executable path");
+            }
+        }
+    }
+}
